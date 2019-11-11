b14521e5f4f40bcb6c3b0d4663d255d1ceaee011
Havid <great_havid@yahoo.co.id>
2019-11-11 14:18:16 +0700

    Tugas 2 JS

---
+++ tugas2.js
@@ -0,0 +1,10 @@
+// ====== Tugas 2 JavaScript =====
+
+const number = 25;
+let string = 'Nilai String';
+var boolan = false;
+let typeNull = null;
+var undefined;
+const array = ["hijau", "biru", "merah", "hitam"];
+
+console.log(number, string, boolan, typeNull, undefined, array);
